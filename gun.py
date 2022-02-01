0abeac9bc85d6906497708ad0730a988b4c2c185
bds23456 <bds2@mail.ru>
2022-02-01 22:18:57 +0300

    gun.py is running, but need to fix some defs

--- gun.py
+++ gun.py
@@ -9,6 +9,68 @@ WIDTH = 800
 HEIGHT = 600
 
 
+class Gun:
+    def __init__(self, screen):
+        self.screen = screen
+        self.f2_power = 10
+        self.f2_on = 0
+        self.an = 1
+        self.x = 40
+        self.y = 450
+        self.width = 10        
+        self.color = GREY
+
+    def fire2_start(self, event):
+        self.f2_on = 1
+        if event.type == pygame.MOUSEBUTTONDOWN :
+            self.power_up()
+            self.draw()
+
+
+    def fire2_end(self, event):
+        """Выстрел мячом.
+
+        Происходит при отпускании кнопки мыши.
+        Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
+        """
+        global balls, bullet
+        bullet += 1
+        new_ball = Ball(self.screen)
+        new_ball.r += 5
+        self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
+        new_ball.vx = self.f2_power * math.cos(self.an)
+        new_ball.vy = - self.f2_power * math.sin(self.an)
+        balls.append(new_ball)
+        self.f2_on = 0
+        self.f2_power = 10
+
+    def targetting(self, event):
+        """Прицеливание. Зависит от положения мыши."""
+        if event:
+            self.an = math.atan((event.pos[1]-450) / (event.pos[0]-40))
+        if self.f2_on:
+            self.color = RED
+        else:
+            self.color = GREY
+
+    def draw(self):
+        pygame.draw.polygon(self.screen, 
+                            self.color, 
+                            [[self.x, self.y], 
+                            [self.x + self.width, self.y+ self.width], 
+                            [self.x + self.f2_power, self.y + self.f2_power],
+                            [self.x + self.width + self.f2_power, self.y+ self.width + self.f2_power]]) 
+
+        # FIXIT don't know how to do it
+
+    def power_up(self):
+        if self.f2_on:
+            if self.f2_power < 100:
+                self.f2_power += 2
+            self.color = RED
+        else:
+            self.color = GREY
+
 class Ball:
     def __init__(self, screen: pygame.Surface, x=40, y=450):
         """ Конструктор класса ball
@@ -20,7 +82,7 @@ class Ball:
         self.screen = screen
         self.x = x
         self.y = y
-        self.r = 10
+        self.r = 20
         self.vx = 0
         self.vy = 0
         self.color = choice(GAME_COLORS)
@@ -53,57 +115,7 @@ class Ball:
         Returns:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
-        # FIXME
-            return False
-
-
-class Gun:
-    def __init__(self, screen):
-        self.screen = screen
-        self.f2_power = 10
-        self.f2_on = 0
-        self.an = 1
-        self.color = GREY
-
-    def fire2_start(self, event):
-        self.f2_on = 1
-
-    def fire2_end(self, event):
-        """Выстрел мячом.
-
-        Происходит при отпускании кнопки мыши.
-        Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
-        """
-        global balls, bullet
-        bullet += 1
-        new_ball = Ball(self.screen)
-        new_ball.r += 5
-        self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
-        new_ball.vx = self.f2_power * math.cos(self.an)
-        new_ball.vy = - self.f2_power * math.sin(self.an)
-        balls.append(new_ball)
-        self.f2_on = 0
-        self.f2_power = 10
-
-    def targetting(self, event):
-        """Прицеливание. Зависит от положения мыши."""
-        if event:
-            self.an = math.atan((event.pos[1]-450) / (event.pos[0]-20))
-        if self.f2_on:
-            self.color = RED
-        else:
-            self.color = GREY
-
-    def draw(self):
-        # FIXIT don't know how to do it
-
-    def power_up(self):
-        if self.f2_on:
-            if self.f2_power < 100:
-                self.f2_power += 1
-            self.color = RED
-        else:
-            self.color = GREY
+        return False
 
 
 class Target:
@@ -114,9 +126,9 @@ class Target:
 
     def new_target(self):
         """ Инициализация новой цели. """
-        x = self.x = rnd(600, 780)
-        y = self.y = rnd(300, 550)
-        r = self.r = rnd(2, 50)
+        x = self.x = math.rnd(600, 780)
+        y = self.y = math.rnd(300, 550)
+        r = self.r = math.rnd(2, 50)
         color = self.color = RED
 
     def hit(self, points=1):
@@ -124,7 +136,7 @@ class Target:
         self.points += points
 
     def draw(self):
-        ...
+        pass
 
 
 pygame.init()
