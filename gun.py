745fa5b073d45395e9e698fc648fda7773a30c66
bds23456 <bds2@mail.ru>
2022-02-03 01:44:13 +0300

    done multiple targets, moving targets, brought beauty

--- gun.py
+++ gun.py
@@ -1,6 +1,5 @@
 import math
-from random import choice
-from random import randint
+from random import choice, randint
 import pygame
 from color import *
 
@@ -27,7 +26,6 @@ class Gun:
             self.power_up()
             self.draw()
 
-
     def fire2_end(self, event):
         """Выстрел мячом.
 
@@ -39,16 +37,26 @@ class Gun:
         new_ball = Ball(self.screen)
         new_ball.r += 5
         self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
-        new_ball.vx = self.f2_power * math.cos(self.an)
-        new_ball.vy = - self.f2_power * math.sin(self.an)
+        new_ball.vx = self.f2_power * math.cos(self.an) / level
+        new_ball.vy = - self.f2_power * math.sin(self.an) / level
         balls.append(new_ball)
         self.f2_on = 0
         self.f2_power = 10
+    
+    def power_up(self):
+        if self.f2_on:
+            if self.f2_power < 100:
+                self.f2_power += 5
+            else :
+                self.f2_power = 100
+            self.color = RED
+        else:
+            self.color = GREY
 
     def targetting(self, event):
         """Прицеливание. Зависит от положения мыши."""
         if event:
-            if event.pos[0]-40 == 0 :
+            if event.pos[0]-40 == 0 : #division by 0 defence
                 self.an = 45
             else :
                 self.an = math.atan((event.pos[1]-450) / (event.pos[0]-40))
@@ -61,29 +69,29 @@ class Gun:
     def draw(self):
         pygame.draw.polygon(self.screen, 
                             self.color, 
-                            [(self.x + self.f2_power, self.y + self.f2_power), 
-                            (self.x, self.y),
-                            (self.x + 20, self.y),
-                            (self.x + 20 + self.f2_power, self.y + self.f2_power)]) 
+                            [(self.x, self.y), 
+                            (self.x, self.y + 10),
+                            (self.x + 10 + self.f2_power, self.y + 10 - self.f2_power),
+                            (self.x + 10 + self.f2_power, self.y - self.f2_power)]) 
 
-        # FIXIT don't know how to do it
+
+    # FIXIT don't know how to do it
     def rotate(self, angle):        
         angle = (180 / math.pi) * -angle
         self.image = pygame.transform.rotate(self.screen, int(angle))
-        self.polygon = self.image.get_rect(center=(self.x , self.y))
+        self.polygon = self.image.get_rect()
+        pygame.draw.polygon(self.screen, 
+                            self.color, 
+                            [self.polygon.topleft, 
+                            self.polygon.bottomleft, 
+                            self.polygon.bottomright, 
+                            self.polygon.topright])
 
-    def power_up(self):
-        if self.f2_on:
-            if self.f2_power < 100:
-                self.f2_power += 2
-            self.color = RED
-        else:
-            self.color = GREY
+
 
 class Ball:
     def __init__(self, screen: pygame.Surface, x=40, y=450):
         """ Конструктор класса ball
-
         Args:
         x - начальное положение мяча по горизонтали
         y - начальное положение мяча по вертикали
@@ -91,7 +99,7 @@ class Ball:
         self.screen = screen
         self.x = x
         self.y = y
-        self.r = 17
+        self.r = 15
         self.vx = 0
         self.vy = 0
         self.color = choice(GAME_COLORS)
@@ -112,7 +120,6 @@ class Ball:
         if (self.y > HEIGHT - self.r) or (self.y < self.r) :
             self.vy *= -1
             self.live -= 10
-
 
     def draw(self):
         pygame.draw.circle(
@@ -138,20 +145,25 @@ class Ball:
 class Target:
     def __init__(self, screen: pygame.Surface) :
         self.screen = screen
-        self.x = randint(600, 780)
-        self.y = randint(300, 550)
+        self.x = randint(400, 750)
+        self.y = randint(250, 550)
         self.r = randint(5, 50)
         self.color = RED
+        self.vx = randint(1, 5)
+        self.vy = randint(1, 5)        
         self.live = 1
         self.points = 0
     
     def new_target(self):
         """ Инициализация новой цели. """
-        self.x = randint(600, 780)
-        self.y = randint(300, 550)
-        self.r = randint(2, 50)
+        self.x = randint(400, 750)
+        self.y = randint(250, 550)
+        self.r = randint(5, 50)
         self.color = RED
+        self.vx = randint(1, 5)
+        self.vy = randint(1, 5)
         self.live = 1
+        self.points = 0
 
     def hit(self, points=1):
         """Попадание шарика в цель."""
@@ -165,11 +177,25 @@ class Target:
             self.r
         )
 
+    def move(self):
+        """Переместить цель по прошествии единицы времени.
+
+        Метод описывает перемещение цели за один кадр перерисовки. То есть, обновляет значения
+        self.x и self.y с учетом скоростей self.vx и self.vy, 
+        и стен по краям области движения цели (размер области 400х350).
+        """
+        self.x += self.vx
+        self.y -= self.vy
+        if (self.x < 280 + self.r) or (self.x > 800 - self.r) :
+            self.vx *= -1
+        if (self.y < self.r) or (self.y > 600 - self.r) :
+            self.vy *= -1
+
 def text() :
     textSurfaceObj1 = fontObj.render("Игра \"Пушка\". ", True, BLACK)
     textSurfaceObj2 = fontObj.render("Сделано выстрелов: " + str(bullet), True, BLACK)
-    textSurfaceObj3 = fontObj.render("Попаданий: " + str(target.points), True, BLACK)
-    textSurfaceObj4 = fontObj.render("Счет: " + str(target.points - bullet), True, BLACK)
+    textSurfaceObj3 = fontObj.render("Попаданий: " + str(all_points), True, BLACK)
+    textSurfaceObj4 = fontObj.render("Счет: " + str(all_points - bullet), True, BLACK)
     screen.blit(textSurfaceObj1, [30, 30])
     screen.blit(textSurfaceObj2, [30, 60])
     screen.blit(textSurfaceObj3, [30, 90])
@@ -179,11 +205,15 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Игра \"Пушка\" ")
 bullet = 0
+all_points = 0
+level = 3
 balls = []
+targets = []
 
 clock = pygame.time.Clock()
 gun = Gun(screen)
-target = Target(screen)
+for i in range(level) :
+    targets.append(Target(screen))
 finished = False
 fontObj = pygame.font.Font(None, 30)
 
@@ -191,7 +221,8 @@ while not finished:
     screen.fill(WHITE)
     text()
     gun.draw()
-    target.draw()
+    for t in targets :
+        t.draw()
     for b in balls:
         b.draw()
     pygame.display.update()
@@ -206,15 +237,18 @@ while not finished:
             gun.fire2_end(event)
         elif event.type == pygame.MOUSEMOTION:
             gun.targetting(event)
-
-    for b in balls:
-        b.move()
-        if b.live < 0:
-            balls.remove(b)
-        if b.hittest(target) and target.live:
-            target.live = 0
-            target.hit()
-            target.new_target()
     gun.power_up()
+    for t in targets:
+        t.move()
+        for b in balls:
+            b.move()
+            if b.live < 0:
+                balls.remove(b)
+            if b.hittest(t) and t.live:
+                t.live = 0
+                t.hit()
+                all_points += t.points
+                t.new_target()                
+    
 
 pygame.quit()
